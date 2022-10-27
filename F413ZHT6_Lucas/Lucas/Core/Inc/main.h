6df5ca1090cc35f8bd199513af1535a53369e628
Andrea <64101124+Andrea-hub02@users.noreply.github.com>
2022-10-27 14:54:06 -0700

    Update main.h

--- F413ZHT6_Lucas/Lucas/Core/Inc/main.h
+++ F413ZHT6_Lucas/Lucas/Core/Inc/main.h
@@ -23,7 +23,47 @@
 #define __MAIN_H
 
 #ifdef __cplusplus
-extern "C" {
+#extern "C" 
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_3|GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PF3 PF5 PF10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_5|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC0 PC3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
 #endif
 
 /* Includes ------------------------------------------------------------------*/
@@ -50,6 +90,53 @@ extern "C" {
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
+void StartRead(void *argument)
+{
+  /* VARIABLE DECLARATION */
+  uint8_t tmp_reading = 0;
+
+  /* Infinite loop */
+  for(;;)
+  {
+
+    MSGQUEUE_OBJ_t msg;
+    /* P/S Control HIGH to read from parallel inputs */
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET);
+    osDelay(10);
+
+    /* Read from inputs */
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+    osDelay(10);
+
+    /* Reset pins to start serial transmission */
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_3, GPIO_PIN_RESET);
+
+    /* Read all 8 inputs */
+    for(int i = 0; i < 8; i++) {
+      /* Shift msg to prepare for reading */
+      msg.flags <<= 1;
+
+      /* HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_SET); */
+      /* Set the message content */
+      tmp_reading = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3);
+      msg.flags |= tmp_reading;
+
+
+      /* Shift to next input */
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+      osDelay(50);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+      osDelay(50);
+    }
+
+    /* Send message to queue. Wait forever, retrying, if queue is full */
+    /* HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_SET); */
+    osMessageQueuePut(mid_MsgQueue, &msg, 0U, osWaitForever);
+    /* HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_RESET); */
+
+    osDelay(1000);
+  }
+}
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
